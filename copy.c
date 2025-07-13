e3c12829dfbcc47869db313494d2a603122ac409
Saravana2611 <p.saravanan2611@gmail.com>
2025-07-14 00:20:02 +0530

    Copy done in 2 parts

--- copy.c
+++ copy.c
@@ -4,16 +4,16 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-int openFile(const char* filePath, const int flag)
+int openFile(const char *filePath, const int flag)
 {
 	printf("Called OpenFile\n");
-        int fd = open(filePath, flag, RW_MODE);
-        if (fd < 0)
-        {
-                printf("Error in opening file\n");
+	int fd = open(filePath, flag, RW_MODE);
+	if (fd < 0)
+	{
+		printf("Error in opening file\n");
 		exit(1);
-        }
-        printf("File opened\n");
+	}
+	printf("File opened\n");
 	return fd;
 }
 
@@ -40,38 +40,86 @@ struct stat getFileStats(const int fd)
 		exit(1);
 	}
 
-	printf("File size: %jd bytes\n", (intmax_t) fileStats.st_size);
-	printf("Blocks allocated: %jd\n", (intmax_t) fileStats.st_blocks);
+	printf("File size: %jd bytes\n", (intmax_t)fileStats.st_size);
+	printf("Blocks allocated: %jd\n", (intmax_t)fileStats.st_blocks);
 
 	return fileStats;
 }
 
-void getCopyRange(int fd, ssize_t startBytes, ssize_t endBytes)
+void getCopyRange(struct stat *fileStats, struct Ranges *ranges)
 {
+	int totalFileSize = fileStats->st_size;
+	ranges->startBytes[0] = 0;
+	ranges->endBytes[0] = totalFileSize / 2;
 
+	ranges->startBytes[1] = totalFileSize / 2;
+	ranges->endBytes[1] = totalFileSize;
 }
 
-void copy(const char* file1Path, const char* file2Path)
+void copyFile(int fd1, int fd2, int offset, ssize_t size)
+{
+	printf("Copying File till %zd bytes\n", size);
+	char buffer[BUFFER_SIZE];
+	ssize_t bytesRead, totalBytesCopied = 0;
+
+	// reposition to file reader
+	if (lseek(fd1, offset, SEEK_SET) == -1)
+	{
+		printf("Error in seeking file\n");
+		exit(1);
+	}
+
+	while (totalBytesCopied < size)
+	{
+		ssize_t toRead = BUFFER_SIZE;
+
+		printf("toRead = %ld totalBytesCopied = %ld\n", toRead, totalBytesCopied);
+		if (size - totalBytesCopied < BUFFER_SIZE)
+		{
+			printf("toRead = %ld\n", toRead);			
+			toRead = size - totalBytesCopied;
+		}
+		printf("toRead = %ld\n", toRead);
+
+		bytesRead = read(fd1, buffer, toRead);
+		if (bytesRead < 0)
+		{
+			printf("Error in seeking file\n");
+			exit(1);
+		}
+
+		ssize_t bytesWritten = write(fd2, buffer, bytesRead);
+		if (bytesWritten < 0)
+		{
+			printf("Error in Wrtiting File\n");
+			exit(1);
+		}
+		printf("BytesWritten = %zd\n", bytesWritten);
+		totalBytesCopied += bytesRead;
+	}
+}
+
+void copy(const char *file1Path, const char *file2Path)
 {
 	printf("Called Copy\n");
-	char buffer[BUFFER_SIZE];
-	
+
 	int fd1 = openFile(file1Path, RW_FLAG);
 	struct stat file1Stats = getFileStats(fd1);
 
 	int fd2 = openFile(file2Path, RWC_FLAG);
 	struct stat fil2Stats = getFileStats(fd2);
 
-	ssize_t bytesRead;
-	while ((bytesRead = read(fd1, buffer, BUFFER_SIZE)) > 0)
-	{
-		write(fd2, buffer, bytesRead);
-	}
+	struct Ranges ranges;
+	getCopyRange(&file1Stats, &ranges);
 
-        fil2Stats = getFileStats(fd2);
+	for (int i = 0; i < 2; i++)
+	{
+		printf("startBytes of %d = %zd \n", i, ranges.startBytes[i]);
+		printf("EndButes of %d = %zd \n", i, ranges.endBytes[i]);
+		copyFile(fd1, fd2, ranges.startBytes[i], ranges.endBytes[i] - ranges.startBytes[i]);
+	}
 
 	closeFile(fd1);
 	closeFile(fd2);
 	printf("File Copied\n");
 }
-
--- copy.h
+++ copy.h
@@ -6,8 +6,15 @@ static const int RW_FLAG = O_RDWR;
 static const int RWC_FLAG = O_RDWR | O_CREAT;
 static const mode_t RW_MODE = S_IRUSR | S_IWUSR;
 static const int BUFFER_SIZE = 500;
+// static const int numberOfProcess = 2;
 
-int openFile(const char* filePath, const int flag);
+struct Ranges
+{
+	ssize_t startBytes[2];
+	ssize_t endBytes[2];
+};
+
+int openFile(const char *filePath, const int flag);
 void closeFile(const int fd);
 struct stat getFileStats(const int fd);
-void copy(const char* file1Path, const char* file2Path);
+void copy(const char *file1Path, const char *file2Path);
